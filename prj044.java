0072152f59de45a5ca2a790d75823e46f9c8f446
Gka1024 <ththtlddkswn@gmail.com>
2022-03-06 23:58:49 +0900

    프로그래머스 - 2개 이하로 다른 비트

---
+++ prj044.java
@@ -0,0 +1,40 @@
+package prj44;
+
+public class prj044 {
+	public static long[] under2dif(long[] arr)
+	{
+		long[] ans = new long[arr.length];
+		
+		for(int i = 0; i < arr.length; i++)
+		{
+			StringBuilder sb = new StringBuilder(Long.toBinaryString(arr[i]));
+			boolean isChanged = false;
+			
+			
+			for(int j = sb.length() - 1; j >= 0; j--)
+			{
+				if(sb.charAt(j) == '0')
+				{
+					sb.setCharAt(j, '1');
+					isChanged = true;
+					break;
+				}
+			}
+			if(!isChanged)
+			{
+				sb.insert(0, "1");
+				sb.setCharAt(1, '0');
+			}
+			
+			ans[i] = Long.parseLong(sb.toString(), 2);
+		}
+		
+		return ans;
+	}
+	
+	public static void main(String[] args)
+	{
+		long[] numbers = {1023, 7};
+		under2dif(numbers);
+	}
+}
