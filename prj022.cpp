065094048c45f8dbf61eb08e0726402491479dff
Gka1024 <96609796+Gka1024@users.noreply.github.com>
2022-07-20 23:23:03 +0900

    프로그래머스 - 가장 큰 수
    때론 지름길은 눈에 보이지 않는법. 이번 기회로 단순 스트링을 비교할 수 있음을 알았음.

--- prj022.cpp
+++ prj022.cpp
@@ -4,7 +4,37 @@
 #include <string>
 using namespace std;
 
+
 bool cmpsize(int a, int b)
+{
+    string astring = to_string(a);
+    string bstring = to_string(b);
+
+    return astring + bstring > bstring + astring;
+}
+
+int main()
+{
+    vector<int> v = {70, 0, 0, 1};
+    sort(v.begin(), v.end(), cmpsize);
+    string str = "";
+    
+    for(auto i : v)
+    {
+        str.append(to_string(i));
+    }
+    
+    if(str[0] == '0')
+    {
+        str = "0";
+    }
+    
+    cout << str;
+
+    return 0;
+}
+
+bool cmpsize_old(int a, int b)
 {
     string astring = to_string(a);
     string bstring = to_string(b);
@@ -41,23 +71,3 @@ bool cmpsize(int a, int b)
     return false;
 }
 
-int main()
-{
-    vector<int> v = {70, 0, 0, 1};
-    sort(v.begin(), v.end(), cmpsize);
-    string str = "";
-    
-    for(auto i : v)
-    {
-        str.append(to_string(i));
-    }
-    
-    if(str[0] == '0')
-    {
-        str = "0";
-    }
-    
-    cout << str;
-
-    return 0;
-}
