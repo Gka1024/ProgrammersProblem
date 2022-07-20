0f69a4a05e6104f55c9f08429df5a12fab7ad4a6
Gka1024 <96609796+Gka1024@users.noreply.github.com>
2022-07-20 23:18:13 +0900

    프로그래머스 - 가장 큰 수(미완성2)
    시간 초과는 해결하였으나, 1~6 테스트케이스가 실패함

---
+++ prj022.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+bool cmpsize(int a, int b)
+{
+    string astring = to_string(a);
+    string bstring = to_string(b);
+    
+    if(astring[0] != bstring[0])
+    {
+        return astring[0] > bstring[0];
+    }
+    else
+    {
+        if(astring.size() != bstring.size())
+        {
+            if(astring.size() < bstring.size())
+            {
+                astring.append(bstring.size() - astring.size(), a % 10 + 48);
+            }
+            else
+            {
+                bstring.append(astring.size() - bstring.size(), b % 10 + 48);
+            }
+            
+        }
+        
+        for(int i = 1; i < astring.size(); i++)
+        {
+            if(astring[i] == bstring[i])
+            {
+                continue;
+            }
+            return astring[i] > bstring[i];
+        }
+    }
+    
+    return false;
+}
+
+int main()
+{
+    vector<int> v = {70, 0, 0, 1};
+    sort(v.begin(), v.end(), cmpsize);
+    string str = "";
+    
+    for(auto i : v)
+    {
+        str.append(to_string(i));
+    }
+    
+    if(str[0] == '0')
+    {
+        str = "0";
+    }
+    
+    cout << str;
+
+    return 0;
+}
